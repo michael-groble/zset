8fcecd83bcfec5a88e21ab19fe793a30e25e1035
michael groble <mike@groble.me>
2023-07-04 19:51:35 -0500

    optimize benchmark

--- benches/advancing_time.rs
+++ benches/advancing_time.rs
@@ -1,24 +1,26 @@
 use zset::skip_list_set::SkipListSet;
 
 fn main() {
+    // skip lists are optimized for inserting at head as opposed to tail.
+    // use "negative" time
     let mut list = SkipListSet::new();
-    const TIME_STEPS: usize = 10_000_000;
-    const MEMBERS: usize = 100_000;
-    const STEPS_PER_CLEANUP: usize = 1_000;
+    const TIME_STEPS: i64 = 10_000_000;
+    const MEMBERS: i64 = 100_000;
+    const STEPS_PER_CLEANUP: i64 = 1_000;
     let mut deleted = 0;
     let mut skipped = 0;
     for t in 0..TIME_STEPS {
         let member = t % MEMBERS;
         let cycle = t / MEMBERS;
         if t % STEPS_PER_CLEANUP == 0 {
-            let cutoff = if t > MEMBERS { t - MEMBERS } else { 0 };
-            deleted += list.delete_range_by_score(..cutoff)
+            let cutoff = t - MEMBERS - 1;
+            deleted += list.delete_range_by_score(-cutoff..)
         }
         // let 10% of members "forget" to update half of the time alternating cycles
         if member < MEMBERS / 10 && member % 2 == cycle % 2 {
             skipped += 1;
         } else {
-            list.insert(member, t);
+            list.insert(member, -t);
         }
     }
     println!("skipped: {} deleted: {}", skipped, deleted);
